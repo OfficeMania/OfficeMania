9b4fd623e52b55a34548bfc663556a2860110f02
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2021-06-03 15:41:07 +0200

    fix: serve index.html when opening the website
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,14 +1,11 @@
 import http from "http";
-import serveIndex from 'serve-index';
 import express from "express";
 import cors from "cors";
 import path from 'path';
-import fs from 'fs';
-import { Server } from "colyseus";
-import { monitor } from "@colyseus/monitor";
+import {Server} from "colyseus";
 
-import { TURoom } from "../common/rooms/turoom";
-import { SERVER_PORT } from "./config";
+import {TURoom} from "../common/rooms/turoom";
+import {SERVER_PORT} from "./config";
 
 const app = express();
 
@@ -26,7 +23,7 @@ const gameServer = new Server({
 });
 
 // "Mount" the html folder as the root of the website
-app.use('/', serveIndex(path.join(process.cwd(), "html"), {'icons': true}));
+//app.use('/', serveIndex(path.join(process.cwd(), "html"), {'icons': true}));
 app.use('/', express.static(path.join(process.cwd(), "html")));
 
 /*
@@ -70,7 +67,7 @@ gameServer.define("turoom", TURoom)
  *
  * See: https://docs.colyseus.io/tools/monitor/
  */
-// app.use("/colyseus", monitor());
+//app.use("/colyseus", monitor());
 
 // Start the server
 gameServer.listen(SERVER_PORT);
