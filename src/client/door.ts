d03ac5ed32cc6e4d858ae6d3e6a87d77c46d6e6a
Giulia Woywod <g.woywod@tu-braunschweig.de>
2021-07-02 18:10:04 +0000

    added methods to door.ts

--- src/client/door.ts
+++ src/client/door.ts
@@ -11,17 +11,40 @@ export class door {
     proofIfClosed(playerDirection: number) {
 
         //TODO
+        if(this.isClosed){
+            //if direction is inside the room then return false
+            return true;
+        } else{
+            return false;
+        }
     }
 
     lockDoor(id: string) {
-
+        if(this.isClosed){
+            //error
+        } else{
+            //if you are not allowed to close this door
+            if(this.direction === 5){
+                //error
+            } else{
+                this.playerId = id;
+                this.isClosed = true;
+            }
+        }
         //TODO
-        this.isClosed = true;
     }
 
     openDoor(id: string) {
 
         //TODO
-        this.isClosed = true;
+        if(!this.isClosed){
+            //error
+        } else{
+            if(id === this.playerId){
+                this.isClosed = false;
+            } else{
+                //error
+            }
+        }
     }
 }
