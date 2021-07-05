b16a9de4c6b4c5f987e05dea6f73cb37e03d65df
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2021-07-05 11:41:26 +0200

    refactor: improve turoom.ts
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/common/rooms/turoom.ts
+++ src/common/rooms/turoom.ts
@@ -3,6 +3,8 @@ import {PlayerData, State} from "./schema/state";
 import fs from 'fs';
 import {generateUUIDv4, KEY_CHARACTER, KEY_USERNAME} from "../util";
 
+const path = require('path');
+
 /*
  * See: https://docs.colyseus.io/server/room/
  */
@@ -15,6 +17,9 @@ export class TURoom extends Room<State> {
         const conferenceId = generateUUIDv4();
         const conferencePassword = generateUUIDv4();
 
+        console.debug(`conferenceId:       ${conferencePassword}`);
+        console.debug(`conferencePassword: ${conferencePassword}`);
+
         //TODO rework the conference so that the server can join a conference too, before anyone else, and become moderator to lock the room down with a password
 
         state.conference.id = conferenceId;
@@ -26,36 +31,25 @@ export class TURoom extends Room<State> {
 
 
         //loads paths from assets
-        var files: string[] = fs.readdirSync('./assets/img/characters');
-        for (let path of files) {
-            if (path.includes(".png")) {
-                this.state.playerSpritePaths.push(path)
-            }
-        }
+        fs.readdirSync('./assets/img/characters').filter(file => file.includes(".png")).forEach(file => this.state.playerSpritePaths.push(file));
 
         //loads paths from templates
-        var path = require('path');
         getPaths("./assets/templates", this.state);
 
-        function getPaths(startParth, newState: State) {
-
-            if (!fs.existsSync(startParth)) {
+        function getPaths(startPath, newState: State) {
+            if (!fs.existsSync(startPath)) {
                 return;
             }
-
-            var files = fs.readdirSync(startParth);
-            for (let i = 0; i < files.length; i++) {
-
-                let filename: string = path.join(startParth, files[i]);
-                var stat = fs.lstatSync(filename);
+            fs.readdirSync(startPath).forEach(file => {
+                const filename: string = path.join(startPath, file);
+                const stat = fs.lstatSync(filename);
                 if (stat.isDirectory()) {
                     getPaths(filename, newState);
                 } else if (filename.indexOf("png") >= 0) {
                     newState.templatePaths.push(filename);
                 }
-            }
+            })
         }
-
 
         //receives movement from all the clients
         this.onMessage("move", (client, message) => {
@@ -83,8 +77,6 @@ export class TURoom extends Room<State> {
                     }
                 }
             }
-
-
         });
 
         //receives character changes
@@ -96,7 +88,6 @@ export class TURoom extends Room<State> {
         this.onMessage(KEY_USERNAME, (client, message) => {
             this.state.players[client.sessionId].name = message;
         });
-
 
         this.onMessage("updateParticipantId", (client, message) => {
             this.state.players[client.sessionId].participantId = message; //TODO Maybe let the server join the jitsi conference too (without mic/cam) and then authenticate via the jitsi chat, that a player is linked to a participantId, so that one cannot impersonate another one.
@@ -123,15 +114,15 @@ export class TURoom extends Room<State> {
     }
 
     onDispose() {
+        //Nothing?
     }
 
     //gameloop for server
     update(deltaTime) {
-        for (let [id, player] of this.state.players) {
+        this.state.players.forEach(player => {
             if (player.cooldown > 0) {
                 player.cooldown--;
             }
-
-        }
+        });
     }
 }
