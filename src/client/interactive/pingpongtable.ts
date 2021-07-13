e1e54f622aa3e7f48f1c1bca1d61f8362c664697
Fabian Vizi <fvizi@tu-bs.de>
2021-07-13 14:17:53 +0200

    fix: add consrtuctor to PingPongTable

--- src/client/interactive/pingpongtable.ts
+++ src/client/interactive/pingpongtable.ts
@@ -2,9 +2,13 @@ import { MessageType } from "../../common/util";
 import { checkInputMode } from "../main";
 import { getOurPlayer, getRoom } from "../util";
 import { Interactive } from "./interactive";
+import { Pong } from "./pong";
 
 export class PingPongTable extends Interactive{
-    
+    pong: Pong[];
+    constructor() {
+        super("pingpongtable", false, 2)
+    }
     
     onInteraction() {
         const ourPlayer = getOurPlayer();
