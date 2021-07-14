59c5fbbd97431d72d625a6fdb17dd0b7f770eba2
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2021-07-15 00:53:53 +0200

    refactor: extract duplicate code in ponghandler.ts
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/common/handler/ponghandler.ts
+++ src/common/handler/ponghandler.ts
@@ -1,51 +1,37 @@
 import {Direction, MessageType} from "../util";
 import {PongState} from "../rooms/schema/state";
 
+function onPongMove(message: string, gameState: PongState, pos: number, callback: (newPos: number) => void) {
+    switch (message) {
+        case Direction.UP: {
+            if (pos > 0) {
+                callback(pos - gameState.velocities.at(1));
+            } else {
+                callback(0);
+            }
+            break;
+        }
+        case Direction.DOWN: {
+            if (pos + gameState.sizes.at(1) < 1000) {
+                callback(pos + gameState.velocities.at(1));
+            } else {
+                callback(1000 - gameState.sizes.at(1));
+            }
+            break;
+        }
+    }
+}
+
 export function onPongMessage() {
     this.onMessage(MessageType.MOVE_PONG, (client, message) => {
-        //console.log("move_pong recieved" + message);
         const gameState: PongState = this.state.pongStates[this.getPongGame(client).toString()]
-        //console.log(gameState);
-        if (gameState && gameState.playerIds.at(0) === client.sessionId) {
-            switch (message) {
-                case Direction.UP: {
-                    if (gameState.posPlayerA > 0) {
-                        gameState.posPlayerA -= gameState.velocities.at(1);
-                    } else {
-                        gameState.posPlayerA = 0;
-                    }
-                    break;
-                }
-                case Direction.DOWN: {
-                    if (gameState.posPlayerA + gameState.sizes.at(1) < 1000) {
-                        gameState.posPlayerA += gameState.velocities.at(1);
-                    } else {
-                        gameState.posPlayerA = 1000 - gameState.sizes.at(1);
-                    }
-                    break;
-                }
-            }
-            //console.log(gameState.posPlayerA + " is pos of player a");
-        } else if (gameState && gameState.playerIds.at(1) === client.sessionId) {
-            switch (message) {
-                case Direction.UP: {
-                    if (gameState.posPlayerB > 0) {
-                        gameState.posPlayerB -= gameState.velocities.at(1);
-                    } else {
-                        gameState.posPlayerB = 0;
-                    }
-                    break;
-                }
-                case Direction.DOWN: {
-                    if (gameState.posPlayerB + gameState.sizes.at(1) < 1000) {
-                        gameState.posPlayerB += gameState.velocities.at(1);
-                    } else {
-                        gameState.posPlayerB = 1000 - gameState.sizes.at(1);
-                    }
-                    break;
-                }
-            }
-            //console.log(gameState.posPlayerB + " is pos of player B");
+        if (!gameState) {
+            return;
         }
-    })
+        if (gameState.playerIds.at(0) === client.sessionId) {
+            onPongMove(message, gameState, gameState.posPlayerA, (newPos: number) => gameState.posPlayerA = newPos);
+        } else if (gameState.playerIds.at(1) === client.sessionId) {
+            onPongMove(message, gameState, gameState.posPlayerB, (newPos: number) => gameState.posPlayerB = newPos);
+        }
+    });
 }
