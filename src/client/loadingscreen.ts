db720c200c25230c49e770c2165e448429879085
Fabian Vizi <fvizi@tu-bs.de>
2021-07-18 16:53:28 +0200

    fix: defined writing size as variable and stopped it going behind the buttonbar

--- src/client/loadingscreen.ts
+++ src/client/loadingscreen.ts
@@ -16,7 +16,8 @@ let showLoadingScreen: boolean = true;
 let scale: number;
 const ctx = canvas.getContext("2d");
 const lineThiccness: number = 8;
-const writing: boolean[][] = [[], [],
+const writingGapX: number = 60;
+const writing: boolean[][] = [[],
 [false, false, true, true, false, false, true, true, true, true, false, true, true, true, true, false,  true, false,  true, true, true, true, false, true, true, true, true, false],
 [false, true, false, false, true, false, true, false, false, false, false, true, false, false, false, false, true, false, true, false, false, false, false, true, false, false, false, false,],
 [false, true, false, false, true, false, true, true, true, false, false, true, true, true, false, false, true, false, true, false, false, false, false, true, true, true, false, false,],
@@ -28,7 +29,7 @@ const writing: boolean[][] = [[], [],
 [false, false, false, true, false, true, false, true, false, true, true, true, true, false, true, false, true, true, false, true, false, true, true, true, true, false, false, false,],
 [false, false, false, true, false, false, false, true, false, true, false, false, true, false, true, false, false, true, false, true, false, true, false, false, true, false, false,false,],
 [false, false, false, true, false, false, false, true, false, true, false, false, true, false, true, false, false, true, false, true, false, true, false, false, true, false, false,false,],
-[],[],
+[],
 ]
 export async function initLoading(){
     /*if (canvas.style.display === "none") {
@@ -49,7 +50,7 @@ export async function initLoading(){
     //choose direction, x += dir*(6*width) (0=r, 1=u, 2=l, 3=d)
     let dir: number = 0;
     x += dir * 6 * playerWidth
-    while (showLoadingScreen) {
+    while (loadCounter < 2000) {
 
             //counter animation
         if (loadCounter % 60 <= 10) {
@@ -68,56 +69,64 @@ export async function initLoading(){
 
         spriteX = x;
         spriteY = 2 * playerHeight;
-        console.log("holle :)")
-        writeOfficeMania(ctx);
-        ctx.drawImage(characters["Adam_48x48.png"], spriteX, spriteY, playerWidth, playerHeight, canvas.width- playerSizeX*2, canvas.height -playerSizeY*2, playerSizeX, playerSizeY);
+        if(loadCounter % 100 === 0) {
+            //console.log("holle :)")
+            ctx.fillStyle = "#d0be9c";
+            //ctx.fillRect(0,0, canvas.width, canvas.height)
+            writeOfficeMania(ctx);
+            ctx.drawImage(characters["Adam_48x48.png"], spriteX, spriteY, playerWidth, playerHeight, canvas.width- playerSizeX*2, canvas.height - playerSizeY*1.5, playerSizeX, playerSizeY);
+
+        }
         
         loadCounter++;
-        if(!showLoadingScreen) {
-            return;
-        }
-        showLoadingScreen = false;
     }
-    
 }   
 
 export function setShowLoadingscreen(show: boolean) {
-    console.log("called")
+    //console.log("called")
     showLoadingScreen = show;
 }  
 function writeOfficeMania(ctx: CanvasRenderingContext2D) {
-    let scaleX: number = Math.floor(canvas.width / 28);
-    let scaleY: number = Math.floor(canvas.height / 15);
-    scaleX > scaleY? scale = scaleY: scale = scaleX;
-    console.log("canvas: " + canvas.width + " scale: " + scale);
+    let scaleX: number = Math.floor(canvas.width / writingGapX);
+    let scaleY: number = Math.floor(canvas.height / 13);
+    let compensationX = 0;
+    let compensationY = 0;
+    if (scaleX < scaleY) {
+        scale = scaleX; 
+        compensationX = (writingGapX - 28)/ 2;
+        while (scale*compensationY< 100) {
+            compensationY++;
+        }
+        //console.log(compensationY);
+    }
+    else {
+        scale = scaleY;
+    }
+    //console.log("canvas: " + canvas.width + " scale: " + scale);
     for(let i = 0; i < writing.length; i++) {
         let line = writing[i];
-        console.log(line.length);
+        //console.log(line.length);
         for (let j = 0; j < line.length; j++) {
             
             if (writing[i][j]) {
                 ctx.fillStyle = "white";
-                ctx.fillRect(j*scale, i*scale, scale, scale);
+                ctx.fillRect((j+compensationX)*scale, (i+compensationY)*scale, scale, scale);
                 ctx.fillStyle = "black";
                 if (!writing[i-1][j]) {
-                    ctx.fillRect(j*scale, i*scale, scale, scale/lineThiccness);
+                    ctx.fillRect((j+compensationX)*scale, (i+compensationY)*scale, scale, scale/lineThiccness);
                 }
                 if (!writing[i+1][j]) {
-                    ctx.fillRect(j*scale, i*scale + (lineThiccness - 1)*scale/lineThiccness, scale, scale/lineThiccness);
+                    ctx.fillRect((j+compensationX)*scale, (i+compensationY)*scale + (lineThiccness - 1)*scale/lineThiccness, scale, scale/lineThiccness);
                 }
                 if (!writing[i][j-1]) {
-                    ctx.fillRect(j*scale, i*scale, scale/lineThiccness, scale);
+                    ctx.fillRect((j+compensationX)*scale, (i+compensationY)*scale, scale/lineThiccness, scale);
                 }
                 if (!writing[i][j+1]) {
-                    ctx.fillRect(j*scale + (lineThiccness - 1)*scale/lineThiccness, i*scale, scale/lineThiccness, scale);
+                    ctx.fillRect((j+compensationX)*scale + (lineThiccness - 1)*scale/lineThiccness, (i+compensationY)*scale, scale/lineThiccness, scale);
                 }
             }
             
         }
     }
-} 
-function p(x: number, y: number){
-    
-    
 }
  
