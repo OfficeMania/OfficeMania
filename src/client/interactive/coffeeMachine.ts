da6853f1cd81e47c65c1392f03dded3cedc7f59c
Fabian Vizi <f.vizi@tu-braunschweig.de>
2021-11-04 00:49:24 +0100

    chore: cleanup of coffeemachine.ts

--- src/client/interactive/coffeeMachine.ts
+++ src/client/interactive/coffeeMachine.ts
@@ -1,16 +1,10 @@
-import { Room } from "colyseus.js";
-import {Interactive} from "./interactive";
-import {getInputMode, setInputMode} from "../input";
-import {createCloseInteractionButton, getRoom, InputMode, removeCloseInteractionButton, } from "../util";
-import { MessageType } from "../../common/util";
-import { checkInputMode } from "../main";
-import { State } from "../../common";
+
 import { MachineType} from "../../common/handler/machinehandler";
 import { Machine } from "./machine";
+
 export class CoffeeMachine extends Machine {
 
     constructor(){
         super("Coffee Machine", MachineType.COFFEE);
     }
-
 }
