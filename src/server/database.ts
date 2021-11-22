a9b4ff9b678c569a4fd338d4a4277102024e40b1
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2021-11-22 15:26:42 +0100

    refactor: move User from database.ts into user.ts
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/server/database.ts
+++ src/server/database.ts
@@ -1,7 +1,8 @@
-import { DataTypes, Model, Sequelize } from "sequelize";
+import { Sequelize } from "sequelize";
 import { createNamespace, Namespace } from "cls-hooked";
 import { Transaction } from "sequelize/dist/lib/transaction";
 import { DEBUG } from "./config";
+import User from "./user";
 
 const namespace: Namespace = createNamespace("namespace-officemania-sequelize");
 Sequelize.useCLS(namespace);
@@ -28,24 +29,8 @@ async function syncDatabase(syncMode: SyncMode = SyncMode.DEFAULT): Promise<void
     });
 }
 
-async function initDatabase(): Promise<void> {
-    return withTransaction(() => Promise.all([createOfficeManiaUser(), createTestUser()])).then(users => {
-        const usersCreated: number = users.reduce(
-            (previousValue, currentValue) => previousValue + (currentValue[1] ? 1 : 0),
-            0
-        );
-        if (!users || users.length !== 2) {
-            console.error("Something went wrong when creating the default users");
-        } else if (DEBUG) {
-            console.debug(`${usersCreated} missing default user${usersCreated === 1 ? " was" : "s were"} created`);
-        }
-    });
-}
-
 export async function connectDatabase(syncMode: SyncMode = SyncMode.DEFAULT): Promise<void> {
-    return authenticateDatabase()
-        .then(() => syncDatabase(syncMode))
-        .then(() => initDatabase());
+    return authenticateDatabase().then(() => syncDatabase(syncMode));
 }
 
 export async function disconnectDatabase(): Promise<void> {
@@ -53,7 +38,7 @@ export async function disconnectDatabase(): Promise<void> {
     return sequelize.close();
 }
 
-const sequelize: Sequelize = new Sequelize({
+export const sequelize: Sequelize = new Sequelize({
     dialect: "sqlite",
     storage: "database_test.sqlite",
 });
@@ -64,56 +49,6 @@ export async function withTransaction<T>(autoCallback: (t: Transaction) => Promi
     return sequelize.transaction(autoCallback);
 }
 
-export class User extends Model {}
-
-User.init(
-    {
-        id: {
-            type: DataTypes.UUID,
-            allowNull: false,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-            unique: true,
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-    },
-    {
-        sequelize,
-        modelName: "User",
-        tableName: "user",
-        createdAt: "created_at",
-        updatedAt: "updated_at",
-    }
-);
-
-async function createOfficeManiaUser(): Promise<[User, boolean]> {
-    return User.findOrCreate({
-        where: { username: "officemania" },
-        defaults: {
-            password: "$2b$12$.eGqcfCBgcGfTAcz37bn.e0v9c1jgmxeAeIYld/K7XbOVx6f8158.",
-        },
-    });
-}
-
-async function createTestUser(): Promise<[User, boolean]> {
-    return User.findOrCreate({
-        where: { username: "Test Username" },
-        defaults: { password: "Invalid Bcrypt Password" },
-    });
-}
-
-function getFirst<T>(array: [T, boolean]): T {
+export function getEntity<T>(array: [T, boolean]): T {
     return array[0];
-}
-
-async function findOrCreateUserByUsername(username: string, defaults: any): Promise<User> {
-    return User.findOrCreate({ where: { username }, defaults }).then(getFirst);
 }
--- src/server/user.ts
+++ src/server/user.ts
@@ -1,26 +1,69 @@
-export default interface User {
-    id: string;
-    username: string;
-    password: string;
-}
+import { DataTypes, Model } from "sequelize";
+import { getEntity, sequelize } from "./database";
+import { compare, compareSync } from "bcrypt";
 
-const users: User[] = [];
-
-createUser("officemania", "officemania", "sec-sep21-project"); // TODO Remove this and use proper user storage
-
-export function findUserById(id: string): User {
-    return users.find(user => user.id === id);
-}
-
-export function createUser(id: string, username: string, password: string = undefined): User {
-    const user: User = { id, username, password };
-    users.push(user);
-    return user;
-}
-
-export function isValidPassword(user: User, password: string): boolean {
-    if (!user) {
-        return false;
+export default class User extends Model {
+    private getPassword() {
+        return this["password"];
     }
-    return user.password === password;
+
+    compareSync(password: string): boolean {
+        return compareSync(password, this.getPassword());
+    }
+
+    compare(password: string): Promise<boolean> {
+        return compare(password, this.getPassword());
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            unique: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+    },
+    {
+        sequelize,
+        modelName: "User",
+        tableName: "user",
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+    }
+);
+
+export function findUserById(id: string): Promise<User> {
+    return User.findByPk(id);
+}
+
+export function findUserByUsername(username: string): Promise<User> {
+    return User.findOne({ where: { username } });
+}
+
+export function createUser(username: string, password: string = undefined): Promise<User> {
+    return User.create({ username, password });
+}
+
+export function findOrCreateUserByUsername(username: string, password: string = undefined): Promise<User> {
+    return User.findOrCreate({ where: { username }, defaults: { password } }).then(getEntity);
+}
+
+function createOfficeManiaUser(): Promise<User> {
+    return createUser("officemania", "$2b$12$.eGqcfCBgcGfTAcz37bn.e0v9c1jgmxeAeIYld/K7XbOVx6f8158.");
+}
+
+function createTestUser(): Promise<User> {
+    return createUser("Test Username", "Invalid Bcrypt Password Hash");
 }
