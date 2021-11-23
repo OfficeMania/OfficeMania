a16cdf50d8b8e9127f3e7d821fe0a2f4a05f04f3
Fabian Vizi <f.vizi@tu-braunschweig.de>
2021-11-23 19:04:05 +0100

    feat: preliminary multi-user-notes

--- src/client/interactive/notes.ts
+++ src/client/interactive/notes.ts
@@ -1,7 +1,9 @@
+
 import { Room } from "colyseus.js";
 import { State } from "../../common";
 import { MessageType } from "../../common/util";
-import { createCloseInteractionButton, getOurPlayer, getRoom } from "../util";
+import { checkInputMode } from "../main";
+import { createCloseInteractionButton, getOurPlayer, getRoom, removeCloseInteractionButton } from "../util";
 import { Interactive } from "./interactive";
 
 export class Notes extends Interactive {
@@ -9,50 +11,94 @@ export class Notes extends Interactive {
     static inputs = [" ", "A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l",
     "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z",
     "Ä", "ä", "Ü", "ü", "Ö", "ö", "-", "_", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "'", "#", "+", "=", "*", "/", ".", ":", ",", ";",
-    "?", "!", "%", "&", "(", ")", "<", ">", "|"];
-    ctx: CanvasRenderingContext2D = this.canvas.getContext("2d");
+    "?", "!", "%", "&", "(", ")", "<", ">", "|", "Backspace", "Enter", "ArrowLeft", "ArrowRight"];
+    ctx: CanvasRenderingContext2D;
     static notesID: number = 0;
 
     id: number = 0;
     content: string = "";
 
-    markers = new Map;
+    marker: number = 0;
     
     room: Room<State>;
-
-    doLoop: boolean = false;
-
+    ourPlayer;
+    inputLam = (e) => {
+        this.room.send(MessageType.NOTES_ENTER, e.key);
+        //console.log("sent request to add key");
+    }
     constructor() {
         super("notes", false, 1);
         this.id = Notes.notesID;
         Notes.notesID++;
-        this.markers.set(this.id, "");
+        this.ctx = this.canvas.getContext("2d");
         this.room = getRoom();
-        this.room.send(MessageType.NOTES_CREATE, this.id);
+        this.room.send(MessageType.NOTES_CREATE);
+        this.ourPlayer = getOurPlayer();
         //this.room.send(MessageType.NOTES_SET, [this.id, ""]);
     }
 
     onInteraction() {
         this.canvas.style.visibility = "visible";
+        this.paint();
         createCloseInteractionButton(() => this.leave());
 
-        this.doLoop = true;
-        //this.loop();
+        checkInputMode();
+        this.loop();
 
-        this.room.send(MessageType.NOTES_ENTER, new Message("1", 1))
+        //this.room.send(MessageType.NOTES_ENTER, "B");
+        document.addEventListener("keydown", this.inputLam);
     }
 
     loop() {
-
-    }
-}
-class Message {
-    message: string;
-    id: number;
-    constructor(key: string, id: number) {
-        this.message = key;
-        this.id = id;
+        this.paint();
+        this.drawText();
     }
 
+    leave() {
+        removeCloseInteractionButton();
+        this.canvas.style.visibility = "hidden";
+        checkInputMode();
+        document.removeEventListener("keydown", this.inputLam);
+    }
+    paint() {
+        this.ctx.fillStyle = "black";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.fillStyle = "rgb(239, 245, 203)";
+        this.ctx.fillRect(5, 5, (this.canvas.width / 2) - 5, (this.canvas.height / 2) - 5);
+        this.ctx.fillStyle = "rgb(216, 245, 203)";
+        this.ctx.fillRect((this.canvas.width / 2), 5, (this.canvas.width / 2) - 5, (this.canvas.height / 2) - 5);
+        this.ctx.fillStyle = "rgb(250, 182, 177)";
+        this.ctx.fillRect(5, (this.canvas.height / 2), (this.canvas.width / 2) - 5, (this.canvas.height / 2) - 5);
+        this.ctx.fillStyle = "rgb(188, 247, 247)";
+        this.ctx.fillRect((this.canvas.width/ 2), (this.canvas.height / 2), (this.canvas.width / 2) - 5, (this.canvas.height / 2) - 5);
+    }
 
+    drawText(){
+        let buffer;
+        this.marker = this.room.state.notesState.markers[this.ourPlayer.id];
+        this.content = this.room.state.notesState.content;
+
+        let subs: string[] = [];
+        let lineCounter: number = 0;
+        let prevPos: number = 0;
+        this.room.state.notesState.lengths.forEach((length) => {
+            subs[lineCounter] = this.content.substr(prevPos, length);
+            prevPos += length;
+        });
+
+        this.ctx.fillStyle = "black";
+        this.ctx.font = "25px DejaVu Sans Mono";
+        this.ctx.lineWidth = 3;
+
+        let i = 0;
+        let j = 0;
+        while(i < subs.length) {
+            this.ctx.fillText(subs[i], 100, 100 + (50 * j));
+            j++;
+            i++;
+        }
+    
+        
+
+    }
 }
--- src/common/handler/noteshandler.ts
+++ src/common/handler/noteshandler.ts
@@ -15,11 +15,11 @@ export class NotesHandler implements Handler{
     }
 
     onCreate(options: any) {
-        this.room.onMessage(MessageType.NOTES_CREATE, (client, message) => {this.initNotes(client, message)})
-        this.room.onMessage(MessageType.NOTES_ENTER, (client, message) => {this.enterNotes(client, message.message, message.id)})
-        this.room.onMessage(MessageType.NOTES_MARKER, (client, message) => {this.modifyMarker(client, message.message, message.id)})
-        this.room.state.notesState.content = "a";
-        this.room.state.notesState.lengths.push(1);
+        this.room.onMessage(MessageType.NOTES_CREATE, (client, message) => {this.initNotes(client)})
+        this.room.onMessage(MessageType.NOTES_ENTER, (client, message) => {this.enterNotes(client, message)})
+        this.room.onMessage(MessageType.NOTES_MARKER, (client, message) => {this.modifyMarker(client, message)})
+        this.room.state.notesState.content = "";
+        this.room.state.notesState.lengths.push(0);
         console.log("Notes: " + this.room.state.notesState.content + " " + this.room.state.notesState.lengths.at(0));
     }
     onJoin() {
@@ -32,23 +32,66 @@ export class NotesHandler implements Handler{
         
     }
 
-    private initNotes(client: Client, id: number) {
-        if (this.room.state.notesState.markers[client.id] === null) {
-            this.room.state.notesState.markers[client.id] = 0;
-            console.log(this.room.state.notesState.markers[client.id])
+    private initNotes(client: Client) {
+        this.room.state.notesState.markers.set(client.id, 0);
+        //console.log(this.room.state.notesState.markers.get(client.id))
+    }
+
+    private enterNotes(client: Client, key: string) {
+        //console.log("inserting: " + key);
+        if (key === "Backspace") {
+            if (this.room.state.notesState.markers[client.id] > 0) {
+                this.room.state.notesState.content = this.room.state.notesState.content.substr(0,this.room.state.notesState.markers[client.id] - 1) + this.room.state.notesState.content.substr(this.room.state.notesState.markers[client.id]);
+                this.moveEverything(false, this.room.state.notesState.markers[client.id]);
+            }
+        }
+        //TODO: Special cases for enter, left, right arrow, delete, end?, pos1?
+        //TODO: check for overflowing lines
+        else {
+            let content = this.room.state.notesState.content;
+            this.room.state.notesState.content = content.substr(0,this.room.state.notesState.markers[client.id]) + key + content.substr(this.room.state.notesState.markers[client.id]-1);
+            //console.log(this.room.state.notesState.content);
+            this.moveEverything(true, this.room.state.notesState.markers[client.id]);
         }
     }
 
-    private enterNotes(client: Client, key: string, id: number) {
-        if (key === "backspace" && this.room.state.notesState.markers[client.id] > 0) {
-            this.room.state.notesState.content = this.room.state.notesState.content.substr(0,this.room.state.notesState.markers[client.id] - 1) + this.room.state.notesState.content.substr(this.room.state.notesState.markers[client.id]);
-        }
-        this.room.state.notesState.content = this.room.state.notesState.content.substr(0,this.room.state.notesState.markers[client.id]) + key + this.room.state.notesState.content.substr(this.room.state.notesState.markers[client.id]);
-
-
+    //handles moving all the markers, correcting the line lengths
+    //direction: true = added a key, false: removed a key
+    //frompos: position of marker before insertion/deletion 
+    private moveEverything (direction: boolean, fromPos: number){
+        
+        let currentPos = 0;
+        let counter = 0;
+        //console.log(fromPos)
+        this.room.state.notesState.lengths.forEach((length) => {
+            if (currentPos + length >= fromPos) {
+                //console.log("found in " + counter)
+                if (direction) {
+                    this.room.state.notesState.lengths.setAt(counter, length + 1);  
+                }
+                else {
+                    this.room.state.notesState.lengths.setAt(counter, length - 1);
+                }
+                //this.room.state.notesState.lengths.forEach((i) => {console.log(i)});
+                return;
+            }
+            //console.log("hi")
+            counter++;
+        });
+        this.room.state.notesState.markers.forEach((marker, clientid, map) => {
+            if (marker >= fromPos) {
+                if (direction) {
+                    this.room.state.notesState.markers.set(clientid, marker + 1);
+                }
+                else {
+                    this.room.state.notesState.markers.set(clientid, marker - 1);
+                }
+            }
+        });
+        
+        
     }
-
-    private modifyMarker(client: Client, direction: Direction, id: string) {
+    private modifyMarker(client: Client, direction: Direction) {
         if (direction === Direction.UP) {
             //TODO
         }
