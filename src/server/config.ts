22e62f7f52e87dbf130f5a095cf1b4f8de6e62ad
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2021-12-05 14:01:01 +0100

    chore: edit SESSION_SECRET in config.ts
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/server/config.ts
+++ src/server/config.ts
@@ -4,7 +4,7 @@
 export const IS_DEV = process.env.NODE_ENV !== "production";
 export const DEBUG: boolean = !!process.env.DEBUG;
 
-export const SESSION_SECRET = process.env.SESSION_SECRET || "USE_A_SECURE_RANDOM_KEY";
+export const SESSION_SECRET: string = process.env.SESSION_SECRET || "USE_A_SECURE_RANDOM_KEY_FOR_THE_SESSIONS";
 
 export const SERVER_PORT = Number(process.env.PORT) || 3000;
 export const WEBPACK_PORT = 8085; // For dev environment only
