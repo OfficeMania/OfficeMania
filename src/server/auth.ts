2a4eb15f5ca82564134e6153f87de4f4d7208710
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2021-12-05 13:12:46 +0100

    refactor: create setupRouter function in auth.ts
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -12,8 +12,46 @@ const LdapStrategy = require("passport-ldapauth").Strategy;
 
 const router: Router = Router();
 
+setupRouter();
+
 export function getAuthRouter(): Router {
     return router;
+}
+
+export function setupRouter(): void {
+    router.post(
+        "/login",
+        passport.authenticate(LDAP_OPTIONS ? "ldapauth" : "local", {
+            successRedirect: "/",
+            failureRedirect: "/login",
+            failureFlash: true,
+        })
+    );
+    router.get("/login.css", (req, res) => res.sendFile(path.join(process.cwd(), "public", "login.css")));
+    router.get("/login", (req, res) => res.sendFile(path.join(process.cwd(), "public", "login.html")));
+
+    if (!DISABLE_SIGNUP) {
+        router.post("/signup", connectionEnsureLogin.ensureLoggedOut(), (req, res, next) => {
+            const username: string = req.body.username;
+            const password: string[] = req.body.password;
+            if (password.length !== 2 || password[0] !== password[1]) {
+                return res.redirect("/login");
+            }
+            findUserByUsername(username).then(user => {
+                if (user) {
+                    return;
+                }
+                return createUser(username, password[0]);
+            });
+            return res.redirect("/login");
+        });
+        router.get("/signup", (req, res) => res.sendFile(path.join(process.cwd(), "public", "signup.html")));
+    }
+
+    router.get("/logout", (req, res) => {
+        req.logout();
+        res.redirect("/");
+    });
 }
 
 export function setupAuth(app: Express): void {
@@ -87,43 +125,10 @@ export function setupAuth(app: Express): void {
     app.use(passport.session());
 
     //app.use(flash());
-    if (IS_DEV) {
+    if (!IS_DEV) {
         app.use((req, res, next) => {
             req.isAuthenticated = () => true;
             next();
         });
     }
-    app.post(
-        "/auth/login",
-        passport.authenticate(LDAP_OPTIONS ? "ldapauth" : "local", {
-            successRedirect: "/",
-            failureRedirect: "/auth/login",
-            failureFlash: true,
-        })
-    );
-    app.get("/auth/login.css", (req, res) => res.sendFile(path.join(process.cwd(), "public", "login.css")));
-    app.get("/auth/login", (req, res) => res.sendFile(path.join(process.cwd(), "public", "login.html")));
-
-    if (!DISABLE_SIGNUP) {
-        app.post("/auth/signup", connectionEnsureLogin.ensureLoggedOut(), (req, res, next) => {
-            const username: string = req.body.username;
-            const password: string[] = req.body.password;
-            if (password.length !== 2 || password[0] !== password[1]) {
-                return res.redirect("/auth/login");
-            }
-            findUserByUsername(username).then(user => {
-                if (user) {
-                    return;
-                }
-                return createUser(username, password[0]);
-            });
-            return res.redirect("/auth/login");
-        });
-        app.get("/auth/signup", (req, res) => res.sendFile(path.join(process.cwd(), "public", "signup.html")));
-    }
-
-    app.get("/auth/logout", (req, res) => {
-        req.logout();
-        res.redirect("/");
-    });
 }
