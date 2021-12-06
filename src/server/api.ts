93671f91f0b985eb89c455eb1455ddaf65a2fece
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2021-12-06 21:09:28 +0100

    feat: add endpoint to get a User by id in api.ts
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import User, { findUserById } from "./user";
 
 const router: Router = Router();
 
@@ -9,5 +10,16 @@ export function getApiRouter(): Router {
 }
 
 function setupRouter(): void {
-    //TODO
+    router.get("/test", (req, res) => res.sendStatus(200));
+    router.get("/user/:id", (req, res) => {
+        const id: string = req.params.id;
+        findUserById(id)
+            .then((user: User) => {
+                if (!user) {
+                    return res.sendStatus(404);
+                }
+                res.json({ id: user.getId(), username: user.getUsername(), role: user.getRole() });
+            })
+            .catch(() => res.sendStatus(500));
+    });
 }
