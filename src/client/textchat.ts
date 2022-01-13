f6ab4cd621eca5cac1af595aba5a7b9fc1840831
Fabian Vizi <f.vizi@tu-braunschweig.de>
2022-01-13 23:38:43 +0100

    chore: removed not working notification stuff

--- src/client/textchat.ts
+++ src/client/textchat.ts
@@ -77,9 +77,6 @@ export function initChatListener() {
     });
 
     textchatSendButton.addEventListener("click", () => {
-        if (Notification.permission !== "default") {
-            Notification.requestPermission();
-        }
         sendMessage(textchatArea.value, textchatSelect.selectedOptions[0].value);
         console.log(textchatSelect.selectedOptions[0].innerText)
         textchatArea.value = "";
@@ -101,12 +98,7 @@ export function initChatListener() {
     });
 }
 
-function onChatUpdate(chatDTOs: ChatDTO[]): void {
-    console.log(Notification.permission)
-    const notification = new Notification("Message recieved", {
-        body: "f m l (:"
-    });
-    
+function onChatUpdate(chatDTOs: ChatDTO[]): void {    
     console.debug("chatDTOs:", chatDTOs);
     chatDTOs.forEach(updateChat);
 }
@@ -235,7 +227,6 @@ function updateParticipatingChats(hard?: boolean) {
             textchatSelect.append(option);
         }
     });
-    //TODO dont reset pointer?? hulp
 }
 function clearTextchatSelect() {
     while (textchatSelect.firstChild) {
