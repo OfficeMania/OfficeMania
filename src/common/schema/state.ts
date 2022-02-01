b87c293b8aada9e52d79a0ae7c60ce42d01cfa8a
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2022-02-01 12:56:29 +0100

    refactor: extract NotesState into notes-state.ts
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/common/schema/state.ts
+++ src/common/schema/state.ts
@@ -5,23 +5,7 @@ import { WhiteboardState } from "./whiteboard-state";
 import { PongState } from "./pong-state";
 import { DoorState } from "./door-state";
 import { TodoState } from "./todo-state";
-
-export class NotesState extends Schema {
-    //content in a single line
-    @type({ map: "number" })
-    markersX: MapSchema<number> = new MapSchema<number>();
-    @type({ map: "number" })
-    markersY: MapSchema<number> = new MapSchema<number>();
-    //contents of lines
-    @type({ array: "string" })
-    contents: ArraySchema<string> = new ArraySchema<string>();
-
-    @type({ array: "string" })
-    oldContents: ArraySchema<string> = new ArraySchema<string>();
-
-    @type("boolean")
-    change: boolean;
-}
+import { NotesState } from "./notes-state";
 
 export class ChessState extends Schema {
     @type("string")
