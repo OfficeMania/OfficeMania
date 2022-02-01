e5203e89e15c8f3f7d153b2fec0d8d980fa6c5db
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2022-02-01 12:21:25 +0100

    refactor: move interfaces and classes from server/handler/chatHandler.ts into common/handler/chat.ts
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/server/handler/chatHandler.ts
+++ src/server/handler/chatHandler.ts
@@ -1,52 +1,8 @@
 import { Client, Room } from "colyseus";
-import { generateUUIDv4, MessageType } from "../../common/util";
+import { MessageType } from "../../common/util";
 import { Handler } from "./handler";
 import { PlayerData, State } from "../../common/schema/state";
-
-export interface ChatMessage {
-    timestamp?: string;
-    name?: string;
-    chatId: string;
-    message: string;
-}
-
-export interface ChatDTO {
-    id: string;
-    name: string;
-    users?: string[];
-}
-
-export class Chat {
-    private readonly _id: string;
-    private readonly _users: string[] = [];
-    private readonly _messages: ChatMessage[] = [];
-    private _name: string;
-
-    constructor(name: string, id: string = generateUUIDv4()) {
-        this._id = id;
-        this._name = name;
-    }
-
-    get id(): string {
-        return this._id;
-    }
-
-    get name(): string {
-        return this._name;
-    }
-
-    set name(value: string) {
-        this._name = value;
-    }
-
-    get users(): string[] {
-        return this._users;
-    }
-
-    get messages(): ChatMessage[] {
-        return this._messages;
-    }
-}
+import { Chat, ChatDTO, ChatMessage } from "../../common/handler/chat";
 
 export class ChatHandler implements Handler {
     room: Room<State>;
