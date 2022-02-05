27ce441d1fb7983a1ec6df4c7037a1af02e97239
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2022-02-05 14:39:08 +0100

    feat: add functions createOrUpdate and createOrUpdateMultiple to database.ts
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/server/database/database.ts
+++ src/server/database/database.ts
@@ -1,4 +1,4 @@
-import { Connection, createConnection, EntityManager, getConnection } from "typeorm";
+import { Connection, createConnection, DeepPartial, EntityManager, EntityTarget, getConnection } from "typeorm";
 import { SqliteConnectionOptions } from "typeorm/driver/sqlite/SqliteConnectionOptions";
 import { InviteCode } from "./entities/invite-code";
 import { User } from "./entities/user";
@@ -31,4 +31,44 @@ process.on("exit", disconnectDatabase);
 
 export async function withTransaction<T>(autoCallback: (entityManager: EntityManager) => Promise<T>): Promise<T> {
     return getConnection().transaction(autoCallback);
+}
+
+export async function createOrUpdate<Entity>(
+    entityManager: EntityManager,
+    entityClass: EntityTarget<Entity>,
+    entityLike: DeepPartial<Entity>,
+    preloadProcessor?: (created: Entity, preloaded?: Entity) => Entity | undefined
+): Promise<Entity> {
+    const createdEntity: Entity = entityManager.create(entityClass, entityLike);
+    const preloadedEntity: Entity | undefined = await entityManager
+        .preload(entityClass, createdEntity)
+        .then((value: Entity | undefined) => {
+            if (preloadProcessor) {
+                return preloadProcessor(createdEntity, value);
+            }
+            return value;
+        });
+    const savedEntity: Entity = await entityManager.save(preloadedEntity || createdEntity);
+    if (!savedEntity) {
+        throw new Error(`Could not save ${entityClass}`);
+    }
+    return savedEntity;
+}
+
+export async function createOrUpdateMultiple<Entity>(
+    entityManager: EntityManager,
+    entityClass: EntityTarget<Entity>,
+    entityLikes: DeepPartial<Entity>[]
+): Promise<Entity[]> {
+    const createdEntities: Entity[] = entityManager.create(entityClass, entityLikes);
+    const savedEntities: Entity[] = [];
+    for (const createdEntity of createdEntities) {
+        const preloadedEntity: Entity | undefined = await entityManager.preload(entityClass, createdEntity);
+        const savedEntity: Entity = await entityManager.save(preloadedEntity || createdEntity);
+        if (!savedEntity) {
+            throw new Error(`Could not save ${entityClass}`);
+        }
+        savedEntities.push(savedEntity);
+    }
+    return savedEntities;
 }
