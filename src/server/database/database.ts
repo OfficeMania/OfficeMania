f8cff57d6a1192478868bc5b69cb23d1b478ae61
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2022-02-05 16:14:16 +0100

    feat: add support for PostgreSQL to database.ts
    split ConnectionOptions for connectDatabase into createSqliteConnectionOptions and createPostgresConnectionOptions
    
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/server/database/database.ts
+++ src/server/database/database.ts
@@ -1,15 +1,50 @@
 import { Connection, createConnection, DeepPartial, EntityManager, EntityTarget, getConnection } from "typeorm";
+import { ConnectionOptions } from "typeorm/connection/ConnectionOptions";
 import { SqliteConnectionOptions } from "typeorm/driver/sqlite/SqliteConnectionOptions";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 import { InviteCode } from "./entities/invite-code";
 import { User } from "./entities/user";
+import { DB, DB_DATABASE, DB_FILE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME } from "../config";
+import { EntitySchema } from "typeorm/entity-schema/EntitySchema";
+
+const entities: (Function | string | EntitySchema)[] = [InviteCode, User];
+
+function createSqliteConnectionOptions(synchronize: boolean): SqliteConnectionOptions {
+    return {
+        type: "sqlite",
+        entities,
+        synchronize,
+        database: DB_FILE || "database.sqlite",
+    };
+}
+
+function createPostgresConnectionOptions(synchronize: boolean): PostgresConnectionOptions {
+    return {
+        type: "postgres",
+        entities,
+        synchronize,
+        host: DB_HOST || "localhost",
+        port: DB_PORT || 5432,
+        username: DB_USERNAME || "officemania",
+        password: DB_PASSWORD,
+        database: DB_DATABASE || "officemania",
+    };
+}
 
 export async function connectDatabase(synchronize = false): Promise<Connection> {
-    const connectionOptions: SqliteConnectionOptions = {
-        type: "sqlite",
-        database: "database.sqlite",
-        entities: [InviteCode, User],
-        synchronize,
-    };
+    let connectionOptions: ConnectionOptions;
+    switch (DB.toLowerCase()) {
+        case "sqlite":
+        case "sqlite3":
+            connectionOptions = createSqliteConnectionOptions(synchronize);
+            break;
+        case "postgres":
+        case "postgresql":
+            connectionOptions = createPostgresConnectionOptions(synchronize);
+            break;
+        default:
+            throw new Error(`Unsupported Database "${DB}"`);
+    }
     return createConnection(connectionOptions);
 }
 
