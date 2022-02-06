10e64f07eef4b5243fe24851e3fecac9c967e4ad
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2022-02-06 20:03:04 +0100

    feat: create get, put, patch and delete /config routes in api.ts
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import { ensureHasRole, Role, User } from "./database/entity/user";
+import { createOrUpdate } from "./database/database";
+import { ConfigEntry } from "./database/entity/config-entry";
+import { getManager } from "typeorm";
 
 const router: Router = Router();
 
@@ -21,5 +24,45 @@ function setupRouter(): void {
                 res.json({ id: user.id, username: user.username, role: user.role });
             })
             .catch(() => res.sendStatus(500));
+    });
+    router.get("/config", ensureHasRole(Role.ADMIN), (req, res) => {
+        const key = req.query.key;
+        if (!key) {
+            return res.status(500).send("Missing query parameter 'key'");
+        }
+        ConfigEntry.findOne(String(key))
+            .then(configEntry => (configEntry ? res.send(configEntry) : res.sendStatus(404)))
+            .catch(reason => res.status(500).send(reason));
+    });
+    router.put("/config", ensureHasRole(Role.ADMIN), (req, res) => {
+        const key: string | undefined = req.query.key ? String(req.query.key) : undefined;
+        if (!key) {
+            return res.status(500).send("Missing query parameter 'key'");
+        }
+        const value: string | null = req.query.value ? String(req.query.value) : null;
+        createOrUpdate(getManager(), ConfigEntry, { key, value })
+            .then(configEntry => res.send(configEntry))
+            .catch(reason => res.status(500).send(reason));
+    });
+    router.patch("/config", ensureHasRole(Role.ADMIN), (req, res) => {
+        const key: string | undefined = req.query.key ? String(req.query.key) : undefined;
+        if (!key) {
+            return res.status(500).send("Missing query parameter 'key'");
+        }
+        const value: string | null = req.query.value ? String(req.query.value) : null;
+        createOrUpdate(getManager(), ConfigEntry, { key, value })
+            .then(configEntry => res.send(configEntry))
+            .catch(reason => res.status(500).send(reason));
+    });
+    router.delete("/config", ensureHasRole(Role.ADMIN), (req, res) => {
+        const key = req.query.key;
+        if (!key) {
+            return res.status(500).send("Missing query parameter 'key'");
+        }
+        ConfigEntry.findOne(String(key))
+            .then(configEntry =>
+                configEntry ? configEntry.remove().then(value => res.send(value)) : res.sendStatus(404)
+            )
+            .catch(reason => res.status(500).send(reason));
     });
 }
