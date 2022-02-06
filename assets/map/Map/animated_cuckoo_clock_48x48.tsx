2792478642947dda39b8ce66978c3f9490dc2cae
Giulia Woywod <g.woywod@tu-braunschweig.de>
2022-02-06 23:06:17 +0100

    feat: more animations

---
+++ assets/map/Map/animated_cuckoo_clock_48x48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="animated_cuckoo_clock_48x48" tilewidth="48" tileheight="48" tilecount="20" columns="10">
+ <image source="../../templates/Modern_Interiors/3_Animated_objects/48x48/spritesheets/animated_cuckoo_clock_48x48.png" width="480" height="96"/>
+</tileset>
---
+++ assets/map/Map/animated_toaster_48x48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="animated_toaster_48x48" tilewidth="48" tileheight="48" tilecount="22" columns="11">
+ <image source="../../templates/Modern_Interiors/3_Animated_objects/48x48/spritesheets/animated_toaster_48x48.png" width="528" height="96"/>
+</tileset>
