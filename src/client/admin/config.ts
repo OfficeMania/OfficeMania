985d83fac3f508129d5c5ab8b37d2a3028edec6b
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2022-02-08 14:26:59 +0100

    fix: add logging to debug admin/config.ts
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/client/admin/config.ts
+++ src/client/admin/config.ts
@@ -129,6 +129,8 @@ function areChangesUnsaved(): boolean {
 
 function updateCacheState(): void {
     const changesUnsaved: boolean = areChangesUnsaved();
+    console.debug("changesUnsaved:", changesUnsaved);
+    console.debug("cache:", cache);
     noteUnsavedChanges.hidden = !changesUnsaved;
 }
 
