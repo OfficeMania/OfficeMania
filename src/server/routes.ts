8f4ee1b58397f6ead9b4db94c0907a5f7e5dd0cb
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2022-02-10 12:48:53 +0100

    feat: create /version route
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -4,6 +4,8 @@ import { getApiRouter } from "./routes/api";
 import { ensureHasRole, Role } from "./database/entity/user";
 import { getAdminRouter } from "./routes/admin";
 import { getCacheRouter } from "./routes/cache";
+import fs from "fs";
+import path from "path";
 
 export async function setupRoutes(app: Express): Promise<void> {
     await setupAuth(app);
@@ -20,6 +22,7 @@ export async function setupRoutes(app: Express): Promise<void> {
     );
     app.use("/auth", getAuthRouter());
     app.use("/cache", getCacheRouter());
+    setupVersion(app);
 }
 
 export function ensureLoggedIn(setReturnTo = true): express.RequestHandler {
@@ -46,4 +49,36 @@ export function ensureLoggedInOr404(): express.RequestHandler {
         }
         next();
     };
+}
+
+function setupVersion(app: Express): void {
+    const headPath: string = path.join(process.cwd(), ".git", "HEAD");
+    const refPattern: RegExp = /ref: (.+)/;
+    const hashPattern: RegExp = /([0-9a-fA-F]+)/;
+    let head: string | undefined = undefined;
+    if (fs.existsSync(headPath)) {
+        let headContent: string = fs.readFileSync(headPath).toString();
+        const refMatch: RegExpMatchArray = headContent.match(refPattern);
+        if (refMatch && refMatch.length > 0) {
+            const refPath: string = path.join(process.cwd(), ".git", refMatch[1]);
+            if (fs.existsSync(refPath)) {
+                headContent = fs.readFileSync(refPath).toString();
+            } else {
+                headContent = "";
+                console.warn(`File "${refPath}" does not exist`);
+            }
+        }
+        const hashMatch: RegExpMatchArray = headContent.match(hashPattern);
+        if (hashMatch && hashMatch.length > 0) {
+            head = hashMatch[1];
+        }
+    } else {
+        console.warn(`File "${headPath}" does not exist`);
+    }
+    app.use("/version", (req, res) => {
+        if (!head) {
+            return res.status(404).send({});
+        }
+        res.send({ head, repo: process.env.GIT_REPO });
+    });
 }
