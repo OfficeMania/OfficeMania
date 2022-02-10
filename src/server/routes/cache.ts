d148309914cafcf4dbe6d8cc0e9356082d5e5aaf
Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>
2022-02-10 21:23:00 +0100

    fix: rework cache to fallback onto original source
    Signed-off-by: Paul Hagedorn <paul.hagedorn@tu-braunschweig.de>

--- src/server/routes/cache.ts
+++ src/server/routes/cache.ts
@@ -1,41 +1,65 @@
 import { Router } from "express";
 import path from "path";
 import fs from "fs";
+import * as url from "url";
 
 const fetch = require("node-fetch");
 
 const router: Router = Router();
 
-setupRouter();
+setupRouter().catch(reason => console.error(reason));
 
 export function getCacheRouter(): Router {
     return router;
 }
 
-function setupRouter(): void {
-    setup("bootstrap.min.css", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
-    setup("bootstrap.bundle.min.js", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js");
-    setup("fontawesome/css/all.css", "https://use.fontawesome.com/releases/v5.15.4/css/all.css");
-    router.get("/fontawesome/*", (req, res, next) => {
-        const subPath: string = req.url.substring("/fontawesome/".length);
-        return res.redirect(`https://use.fontawesome.com/releases/v5.15.4/${subPath}`);
+async function setupRouter(): Promise<void> {
+    await setupCache("https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
+    await setupCache("https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js");
+    await setupCache("https://use.fontawesome.com/releases/v5.15.4/css/all.css", true);
+    const temp: string = "/use.fontawesome.com/";
+    router.get(temp + "*", (req, res, next) => {
+        const subPath: string = req.url.substring(temp.length);
+        return res.redirect(`https://use.fontawesome.com/${subPath}`);
     });
 }
 
-function setup(name: string, source: string, maxAge = 86400000): void {
-    const filePath: string = path.join(process.cwd(), "cache", name);
+async function setupCache(source: url.URL | string, recursive = false, maxAge = 86400000): Promise<void> {
+    if (typeof source === "string") {
+        source = new url.URL(source);
+    }
+    const nameRaw: string = source.host + source.pathname;
+    const nameSanitized: string = nameRaw.replace(/[^a-z0-9.\-@/]/gi, "_");
+    const filePath: string = path.join(process.cwd(), "cache", nameSanitized);
     const parentPath: string = filePath.substring(0, Math.max(filePath.lastIndexOf("/"), filePath.lastIndexOf("\\")));
-    router.get(`/${name}`, (req, res) => res.sendFile(filePath, { maxAge }));
     if (!fs.existsSync(parentPath)) {
         fs.mkdirSync(parentPath, { recursive: true });
     }
-    if (!fs.existsSync(filePath)) {
-        fetch(source)
-            .then(response => response.text())
-            .then(data => {
-                fs.writeFileSync(filePath, data);
-                console.debug(`Cached ${name} ${data.length / 1000} KB from ${source}`);
-            })
-            .catch(reason => console.error(`Failed to cache ${name} from ${source}`, reason));
+    if (!fs.existsSync(parentPath)) {
+        setupRedirect(source, recursive);
+        return;
     }
+    if (!fs.existsSync(filePath)) {
+        const data: string | undefined = await fetch(source)
+            .then(response => response.text())
+            .catch(reason => console.error(`Failed to cache ${nameSanitized} from ${source}`, reason));
+        if (data) {
+            fs.writeFileSync(filePath, data);
+            console.debug(`Cached ${nameSanitized} ${data.length / 1000} KB from ${source}`);
+        }
+    }
+    if (!fs.existsSync(filePath)) {
+        setupRedirect(source, recursive);
+        return;
+    }
+    router.get(`/${nameSanitized}`, (req, res) => res.sendFile(filePath, { maxAge }));
+}
+
+function setupRedirect(source: url.URL | string, recursive = false): void {
+    if (typeof source === "string") {
+        source = new url.URL(source);
+    }
+    const nameRaw: string = source.host + source.pathname;
+    const nameSanitized: string = nameRaw.replace(/[^a-z0-9.\-@/]/gi, "_");
+    router.get(`/${nameSanitized}`, (req, res) => res.redirect(source.toString()));
 }
