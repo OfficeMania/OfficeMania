bb843981565980b7d23bf9300a12c700146a2f5b
Michael_Goslar <m.goslar@tu-braunschweig.de>
2022-02-13 00:59:36 +0100

    fix: merge conflict

--- src/client/main.ts
+++ src/client/main.ts
@@ -93,11 +93,7 @@ import { getInFocus, initChatListener } from "./textchat";
 import { Backpack } from "./backpack";
 import { literallyUndefined, MessageType } from "../common/util";
 import { State } from "../common";
-<<<<<<< HEAD
-import {createMapJson, drawChairsAbovePlayers, drawMap, GroundType, TileList} from "./newMap";
-=======
-import { createMapJson, drawMap } from "./newMap";
->>>>>>> 1cfb5b5dc0aa8494db6a8a2a77aec5171fd8c6a8
+import { createMapJson, drawChairsAbovePlayers, drawMap } from "./newMap";
 import { Space } from "./util/space";
 
 export const characters: { [key: string]: AnimatedSpriteSheet } = {};
